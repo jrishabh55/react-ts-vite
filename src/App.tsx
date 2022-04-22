f36054f4be2057963adcaa3faa1902459fdf3b0f
Rishabh Jain <temp-rishabh.jain@verse.in>
2022-04-22 18:47:24 +0530

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center bg-teal-900 text-center text-4xl text-white">
+      Boiler plate for a perfect app.
+    </div>
+  );
+}
+
+export default App;
